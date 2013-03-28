68ae41f909cc8964847a490358b49878241b43f5
Rehno Lindeque <errantkid@gmail.com>
2013-03-28 13:50:44 -0600

    Add the license to glQuery-ext

---
+++ src/header.js
@@ -0,0 +1,5 @@
+/*
+ * glQuery-ext - A collection of extensions for a fluent WebGL engine (https://github.com/glQuery)
+ * glQuery-ext is free, public domain software (http://creativecommons.org/publicdomain/zero/1.0/)
+ * Originally created by Rehno Lindeque of http://www.mischievousmeerkat.com
+ */
